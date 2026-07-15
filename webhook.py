1e96631d6c82c794392de80df4e84c8d24285cf9
umarsaraki <umarsaraki81@gmail.com>
2026-07-15 17:29:24 +0100

    Replace SUPABASE_SERVICE_ROLE_KEY with SUPABASE_SECRET_KEY

--- webhook.py
+++ webhook.py
@@ -5,7 +5,7 @@ from supabase import create_client
 app = Flask(__name__)
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
-SUPABASE_SERVICE_ROLE_KEY = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_SECRET_KEY = os.environ.get("SUPABASE_SECRET_KEY")
 FLW_SECRET_KEY = os.environ.get("FLW_SECRET_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
